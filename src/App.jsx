731061b662b29f78cbc4433cd68accf7db72651a
NhiThanhLuong <luongthanhnhi00@gmail.com>
2021-10-22 11:12:47 +0700

    react-simen-demo link

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,10 @@ function App() {
         <Route exact path="/">
           <Container/>
         </Route>
+        <Route path='/react-simen-demo' component={() => { 
+            window.location.href = '/'; 
+            return null;
+        }}/>
         <Route path="/product/:id">
           <ProductItem/>
         </Route>
